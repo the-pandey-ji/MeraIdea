02e02ca9c5bd6c7d213c1cea270531433b5ca2cd
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-10-26 16:48:02 +0530

    html part added to register.php

--- register.php
+++ register.php
@@ -83,51 +83,39 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 <body>
 
-    <h2>Modal Signup Form</h2>
-
-    <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Sign Up</button>
-
-    <div id="id01" class="modal">
-        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-        <form class="modal-content" action="/action_page.php">
-            <div class="container">
-                <h1>Sign Up</h1>
-                <p>Please fill in this form to create an account.</p>
-                <hr>
-                <label for="email"><b>Email</b></label>
-                <input type="text" placeholder="Enter Email" name="email" required>
-
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" required>
-
-                <label for="psw-repeat"><b>Repeat Password</b></label>
-                <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-
-                <label>
-                    <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-                </label>
-
-                <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
-
-                <div class="clearfix">
-                    <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-                    <button type="submit" class="signupbtn">Sign Up</button>
-                </div>
-            </div>
-        </form>
+<div id="id02" class="modal">
+      <form class="modal-content" action="" method="POST">      
+        <div class="imgcontainer">
+        <span onclick="document.getElementById('id02').style.display='block'" class="close" title="Close">&times;</span>
+        <img src="photos/avatar.png" alt="Avatar" style="width: 10%;" class="avatar">
+      </div>
+        <div class="container">
+          <h1 style="  text-align: center;">Sign Up</h1>
+          <p style="  text-align: center;">Please fill in this form to create an account.</p>
+          <hr>
+          <label for="username"><b>Username</b></label>
+          <input type="text" placeholder="User Name" name="username" required>
+    
+          <label for="password"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="password" required>
+    
+          <label for="confirm_password"><b>Repeat Password</b></label>
+          <input type="password" placeholder="Repeat Password" name="confirm_password" required>
+          
+          <label>
+            <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+          </label>
+    
+          <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+    
+          <div class="clearfix">
+            <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
+            <button type="submit" class="signupbtn">Sign Up</button>
+          </div>
+        </div>
+      </form>
     </div>
 
-    <script>
-        // Get the modal
-        var modal = document.getElementById('id01');
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-    </script>
 
 </body>
 
