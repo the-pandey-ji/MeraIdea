ace850bdef30c9273b41ca29a2cab39d5e19184c
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-10-26 18:37:24 +0530

    wrong password server error remains for solving

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 // check if the user is already logged in
@@ -13,7 +14,8 @@ $username = $password = "";
 $err = "";
 
 // if request method is post
-if ($_SERVER['REQUEST_METHOD'] == "POST"){
+if ($_SERVER['REQUEST_METHOD'] == "POST")
+{
     if(empty(trim($_POST['username'])) || empty(trim($_POST['password'])))
     {
         $err = "Please enter username and password";
@@ -24,8 +26,8 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
     }
 
 
-if(empty($err))
-{
+    if(empty($err))
+    {
     $sql = "SELECT id, username, password FROM users WHERE username = ?";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "s", $param_username);
@@ -33,7 +35,8 @@ if(empty($err))
     
     
     // Try to execute this statement
-    if(mysqli_stmt_execute($stmt)){
+    if(mysqli_stmt_execute($stmt))
+    {
         mysqli_stmt_store_result($stmt);
         if(mysqli_stmt_num_rows($stmt) == 1)
                 {
@@ -53,19 +56,17 @@ if(empty($err))
                             
                         }
                         else{
-                        echo "Yourname or Password not matched!!";
+                      
                         header("location : login.php");
                         }
-                    }
 
+                    }
+                    
                 }
 
     }
 }    
-
-
 }
-
 
 ?>
 
@@ -73,38 +74,38 @@ if(empty($err))
 
 
 <!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="login.css">
-</head>
-<body>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="login.css">
+    </head>
+    <body>
 
-<div id="id01" class="modal" style="display: block;">
+        <div id="id01" class="modal" style="display: block;">
   
-      <form class="modal-content animate" action="" method="post">
-        <div class="imgcontainer">
-          <img src="photos/avatar.png" alt="Avatar" class="avatar">
-        </div>
+            <form class="modal-content animate" action="" method="post">
+              <div class="imgcontainer">
+                <img src="photos/avatar.png" alt="Avatar" class="avatar">
+              </div>
+  
+                <div class="container">
+                    <label for="uname"><b>Username</b></label>
+                    <input type="text" placeholder="Enter Username" name="username" required>
     
-        <div class="container">
-          <label for="uname"><b>Username</b></label>
-          <input type="text" placeholder="Enter Username" name="username" required>
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="password" required>
     
-          <label for="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="password" required>
-            
-          <button type="submit">Login</button>
-          <label>
-            <input type="checkbox" checked="checked" name="remember"> Remember me
-          </label>
+                    <button type="submit">Login</button>
+                    <label>
+                      <input type="checkbox" checked="checked" name="remember"> Remember me
+                    </label>
+                </div>
+  
+                <div class="container" style="background-color:#f1f1f1">
+                    <button type="button" onclick="document.getElementById('id01').style.display='block'" class="cancelbtn">Cancel</button>
+                    <span class="psw">Forgot <a href="#">password?</a></span>
+                </div>
+            </form>
         </div>
-    
-        <div class="container" style="background-color:#f1f1f1">
-          <button type="button" onclick="document.getElementById('id01').style.display='block'" class="cancelbtn">Cancel</button>
-          <span class="psw">Forgot <a href="#">password?</a></span>
-        </div>
-      </form>
-    </div>
-</body>
+    </body>
 </html>
